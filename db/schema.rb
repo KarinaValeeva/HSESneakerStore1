922fb51de84456245f5a4ed97f864195a2ed366d
KarinaValeeva <valeevakarina@mail.ru>
2017-05-10 02:50:52 +0300

    Deleted database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507222351) do
+ActiveRecord::Schema.define(version: 20170508204338) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
     t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sneakers", force: :cascade do |t|
+    t.integer "brand_id"
+    t.string "model"
+    t.string "sex"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,76 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Rake::Task['db:drop'].invoke
+Rake::Task['db:create'].invoke
+Rake::Task['db:migrate'].invoke
+
+def random_brand_id
+  Brand.offset(rand(Brand.count)).first.id
+end
+
+def upload_fake_image
+  uploader = ImageUploader.new(Sneaker.new, :image)
+  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'lib/tasks/sneakers', '*')).sample))
+  uploader
+
+end
+
+def upload_fake_logo
+  uploader = LogoUploader.new(Brand.new, :logo)
+  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'lib/tasks/brands', '*')).sample))
+  uploader
+
+end
+
+@sneaker_models = ['Model_1', 'Model_2', 'Model_3', 'Model_4', 'Model_5', 'Model_6', 'Model_7']
+
+def sneaker_price
+  rand(2000..29865)
+end
+
+@sneaker_sex = ['F', 'M']
+
 Brand.create([
   {
-    name: 'NIKE'
+    name: 'NIKE',
+    logo: upload_fake_logo
 
   },{
 
-    name: 'ADIDAS'
+    name: 'ADIDAS',
+    logo: upload_fake_logo
 
   },{
 
-    name: 'REEBOK'
+    name: 'REEBOK',
+    logo: upload_fake_logo
 
   },{
 
-    name: 'PUMA'
+    name: 'PUMA',
+    logo: upload_fake_logo
 
   },{
 
-    name: 'MERRELL'
+    name: 'New Balance',
+    logo: upload_fake_logo
 
-  },{
-
-    name: 'COLUMBIA'
-
-  },{
-
-    name: 'YONEX'
-
-  },{
-
-    name: 'TIMBERLAND'
   }
+
 ])
+
+def create_sneaker
+  Sneaker.create(
+    brand_id: random_brand_id,
+    image: upload_fake_image,
+    model: @sneaker_models.sample,
+    sex: @sneaker_sex.sample,
+    price: sneaker_price
+  )
+end
+
+50.times do
+  sneaker = create_sneaker
+  puts "Sneaker #{sneaker.id} created"
+end
